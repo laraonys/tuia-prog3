c46aa18996e419b2ede22a4b1166b2f5bfd29020
laraonys <lara.onys@gmail.com>
2025-10-19 22:53:07 -0300

    Desarrollo A*

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -22,8 +22,45 @@ class AStarSearch:
         reached = {}
         reached[root.state] = root.cost
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = PriorityQueueFrontier()
+        
+
+        h = abs(root.state[0] - grid.end[0]) + abs(root.state[1] - grid.end[1])
+
+        priority = root.cost + h
+        frontier.add(root, priority)
+
+        # Main loop
+        while not frontier.is_empty():
+            
+            node = frontier.pop()
+
+            # Check if we reached the goal
+            if grid.objective_test(node.state):
+                return Solution(node, reached)
+
+            # Expand current node
+            for action in grid.actions(node.state):
+                successor = grid.result(node.state, action)
+                
+                # Calculate new cost
+                new_cost = node.cost + grid.individual_cost(node.state, action)
+
+                if successor not in reached or new_cost < reached[successor]:
+                    reached[successor] = new_cost
+                    
+                    # Create successor node
+                    son = Node(
+                        "",
+                        successor,
+                        cost=new_cost,
+                        parent=node,
+                        action=action,
+                    )
+                    
+                    # Calculate heuristic for successor
+                    h = abs(successor[0] - grid.end[0]) + abs(successor[1] - grid.end[1])
+                    priority = new_cost + h
+                    frontier.add(son, priority)
 
         return NoSolution(reached)
